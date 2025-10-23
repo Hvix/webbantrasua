ded6aa30cd6ae37c7c271845d75f64e7e735600b
Hvix <164524164+Hvix@users.noreply.github.com>
2025-10-23 21:20:23 +0700

    Add files via upload

---
+++ configs/user_footer.php
@@ -0,0 +1,46 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email.png" alt="">
+         <h3>Email</h3>
+         <a href="mailto:hvt@gmail.com">hvt@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/timetable.png" alt="">
+         <h3>Mở cửa</h3>
+         <p>7h - 20h</p>
+      </div>
+
+      <div class="box">
+         <img src="images/location-pin.png" alt="">
+         <h3>Địa chỉ</h3>
+         <a href="#">TP Hồ Chí Minh </a>
+      </div>
+
+      <div class="box">
+         <img src="images/contacts.png" alt="">
+         <h3>Liên hệ</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+      </div>
+
+   </section>
+
+   <div class="credit"> <span>Trà Sửa HVT</span> </div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
+
+
+<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+<df-messenger
+  intent="WELCOME"
+  chat-title="chat_box"
+  agent-id="e961fe9a-7c27-4adc-be1a-208cb0d0a821"
+  language-code="vi"
+></df-messenger>
